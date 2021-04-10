04df66aad02f85944e534a9b0b26a844811f1209
simon <parkseungchul@gmail.com>
2021-04-10 16:31:02 +0900

    first

---
+++ services/composite/src/test/java/com/psc/cloud/composite/CompositeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psc.cloud.composite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
